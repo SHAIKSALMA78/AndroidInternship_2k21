c9d097137c81cbd7d8473335b8dd5b6dfafebe52
sallu <salma@gmail.com>
2021-06-14 21:43:21 +0530

    files added

---
+++ Login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void login(View view) {
+    }
+
+    public void register(View view) {
+    }
+}
